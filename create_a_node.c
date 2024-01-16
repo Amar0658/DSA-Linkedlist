a9e08f3b108ab0c8cb4aca9538ea7f0df8970073
Amar0658 <137255721+Amar0658@users.noreply.github.com>
2024-01-17 01:32:36 +0530

    Update linkedlist.c

--- create_a_node.c
+++ create_a_node.c
@@ -1,1 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+  int data;
+  struct node *link;
+}node;
 
+node* create_node(int a)
+{
+  node *temp;
+  temp=malloc(sizeof(node));
+  temp->data=a;
+  temp->link=NULL;
+  return temp;
+}
+
+void display(node *head)
+{
+  node *ptr;
+  ptr=head;
+  while(ptr!=NULL)
+    {
+      printf("%d",ptr->data);
+      ptr=ptr->link;
+    }
+}
+
+int count_node()
+{
+  node *ptr;
+  ptr=head;
+  int count=0;
+  while(ptr!=NULL)
+    {
+      ptr=ptr->link;
+      count++;
+    }
+  return count;
+}
+
+void insert_at_beginning(int x)
+{
+  node *temp;
+  temp=malloc(sizeof(node));
+  if(temp!=null) //because malloc only can return NULL or valid address
+  {
+    temp->data=x;
+    temp->link=head;
+    head=temp;
+  }
+}
+
+
+void insertion_at_last()
+{
+  
+}
+
+int main ()
+{
+  node *head;
+  
+}
